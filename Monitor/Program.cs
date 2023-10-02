abc765f83cf9add45efc22f0670ec016f318653d
Vadim Tarelov <vtarelov@ozon.ru>
2023-10-02 23:55:07 +0300

    [Dining Philosophers] add solutions

---
+++ Monitor/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Threading;
+
+
+// Пример ниже показывает решение, где вилки не представляются явно.
+//  Философы могут есть, если ни один из их соседей не ест.
+//   Аналогично системе, где философы, которые не могут взять вторую вилку, должны положить первую вилку до того, как они попробуют снова.
+
+// В отсутствие блокировок, связанных с вилками, философы должны обеспечивать то, что начало принятия пищи не основывается на старой информации о состоянии соседей.
+//  Например: Если философ Б видит, что A не ест в данный момент времени, а потом поворачивается и смотрит на В, 
+//  A мог начать есть, пока философ Б смотрит на В. 
+//  Используя одну взаимоисключающую блокировку (Мьютекс), можно избежать этой проблемы.
+//   Эта блокировка не связана с вилками, но она связана с решением процедур, которые могут изменить состояние философов. Это обеспечивается монитором.
+
+// Алгоритм монитора реализует схему «проверить, взять и положить» и совместно использует взаимоисключающую блокировку. Заметьте, что философы, желающие есть, не будут иметь вилок.
+
+// Если монитор разрешает философу, желающему есть, действовать, то философ снова завладевает первой вилкой, прежде чем взять уже свободную вторую.
+
+// По окончании текущего приёма пищи философ оповещает монитор о том, что обе вилки свободны.
+
+// Стоит заметить, что этот алгоритм монитора не решает проблемы голодания. 
+// Например, философ Б может бесконечно ждать своей очереди, если у философов A и В периоды приёма пищи всё время пересекаются. 
+// Чтобы гарантировать также, что ни один философ не будет голодать, можно отслеживать, сколько раз голодный философ не ел, когда его соседи положили вилки на стол.
+//  Если количество раз превысит некий предел, такой философ перейдёт в состояние
+//   Голодания и алгоритм монитора форсирует процедуру завладения вилками, выполняя условие недопущения голодания ни одного из соседей.
+
+// Философ, не имеющий возможности взять вилки из-за того, что его сосед голодает, находится в режиме полезного ожидания окончания приёма пищи соседом его соседа. 
+// Эта дополнительная зависимость снижает параллелизм. Увеличение значения порога перехода в состояние Голодание уменьшает этот эффект. 
+class DiningPhilosophers
+{
+    static Random random = new Random();
+
+    static void Main()
+    {
+        Console.WriteLine("Dining Philosophers C# with Resource hierarchy");
+
+        DiningPhilosophers dining = new DiningPhilosophers(5);
+        dining.Start();
+
+        Console.ReadLine();
+    }
+
+    public DiningPhilosophers(int count)
+    {
+        sereal = 0;
+        m = new Mutex();
+
+        self = new object[count];
+        for (int i = 0; i < 5; i++)
+        {
+            self[i] = new object();
+        }
+
+        state = new PhilosopherState[count];
+        for (int i = 0; i < 5; i++)
+        {
+            state[i] = PhilosopherState.Thinking;
+        }
+    }
+
+    public void Start()
+    {
+        Thread[] philosopherThreads = new Thread[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            philosopherThreads[i] = new Thread(Task);
+            philosopherThreads[i].Start();
+        }
+
+        foreach (var thread in philosopherThreads)
+        {
+            thread.Join();
+        }
+    }
+
+    public void Task()
+    {
+        int i = sereal;
+        sereal++;
+        while (true)
+        {
+            Think(i);
+            Pickup(i);
+            Eat(i);
+            Putdown(i);
+        }
+    }
+
+
+    private int sereal;
+    private Mutex m;
+    private object[] self;
+    private PhilosopherState[] state;
+
+    private enum PhilosopherState
+    {
+        Thinking,
+        Hungry,
+        Eating
+    }
+
+    private void Think(int id)
+    {
+        int duration = random.Next(200, 800);
+        Console.WriteLine($"{id} thinks for {duration}ms");
+        Thread.Sleep(duration);
+    }
+
+    public void Pickup(int i)
+    {
+        bool shouldSlepp = false;
+
+        m.WaitOne();
+        {
+            state[i] = PhilosopherState.Hungry;
+            Console.WriteLine($"\t\t{i} is hungry");
+            Test(i);
+
+            if (state[i] != PhilosopherState.Eating)
+            {
+                shouldSlepp = true;
+            }
+        }
+        m.ReleaseMutex();
+
+        if (shouldSlepp)
+        {
+            lock (self[i])
+            {
+                Monitor.Wait(self[i]);
+            }
+        }
+    }
+
+    private void Eat(int id)
+    {
+        int duration = random.Next(200, 800);
+        Console.WriteLine($"\t\t\t\t{id} eats for {duration}ms");
+        Thread.Sleep(duration);
+    }
+
+    public void Putdown(int i)
+    {
+        m.WaitOne();
+        {
+            state[i] = PhilosopherState.Thinking;
+            Test((i + 5 - 1) % 5);
+            Test((i + 1) % 5);
+        }
+        m.ReleaseMutex();
+    }
+
+    private void Test(int i)
+    {
+        if (state[(i + 5 - 1) % 5] != PhilosopherState.Eating
+            && state[(i + 1) % 5] != PhilosopherState.Eating
+            && state[i] == PhilosopherState.Hungry)
+        {
+            state[i] = PhilosopherState.Eating;
+            lock (self[i])
+            {
+                Monitor.Pulse(self[i]);
+            }
+        }
+    }
+}
---
+++ ResourceHierarchy/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Threading;
+
+// Описание принципа работы взял с https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B4%D0%B0%D1%87%D0%B0_%D0%BE%D0%B1_%D0%BE%D0%B1%D0%B5%D0%B4%D0%B0%D1%8E%D1%89%D0%B8%D1%85_%D1%84%D0%B8%D0%BB%D0%BE%D1%81%D0%BE%D1%84%D0%B0%D1%85
+// Решение достигается путём присвоения частичного порядка ресурсам (в данном случае вилкам) и
+//  установления соглашения, что ресурсы запрашиваются в указанном порядке, а возвращаются в обратном порядке. 
+//  Кроме того, не должно быть двух ресурсов, не связанных порядком, используемых одной рабочей единицей.
+
+// Пусть ресурсы(вилки) будут пронумерованы от 1 до 5, и каждая рабочая единица (философ) всегда 
+// берёт сначала вилку с наименьшим номером, а потом вилку с наибольшим номером из двух доступных. 
+// Далее, философ кладёт сначала вилку с бо́льшим номером, потом — с меньшим. В этом случае, если четыре из
+//  пяти философов одновременно возьмут вилку с наименьшим номером, на столе останется вилка с наибольшим возможным номером. 
+//  Таким образом, пятый философ не сможет взять ни одной вилки. Более того, только один философ будет иметь доступ к вилке с наибольшим номером,
+//   так что он сможет есть двумя вилками. Когда он закончит использовать вилки, он в первую очередь положит
+//    на стол вилку с бо́льшим номером, потом — с меньшим, тем самым позволив другому философу взять недостающую вилку и приступить к еде.
+
+// В то время, как иерархия ресурсов позволяет избежать взаимных блокировок, данное решение не всегда является практичным,
+//  в особенности когда список необходимых ресурсов неизвестен заранее.
+//   Например, если рабочая единица удерживает ресурс 3 и 5 и решает, что ей необходим ресурс 2,
+//    то она должна выпустить ресурс 5, затем 3, после этого завладеть ресурсом 2 и снова взять ресурс 3 и 5.
+//     Компьютерные программы, которые работают с большим количеством записей в базе данных, 
+//     не смогут работать эффективно, если им потребуется выпускать все записи с верхними индексами прежде,
+//      чем завладеть новой записью. Это делает данный метод непрактичным. 
+
+class DiningPhilosophers
+{
+    static Random random = new Random();
+
+    static void Main()
+    {
+        Console.WriteLine("Dining Philosophers C# with Resource hierarchy");
+
+        int numPhilosophers = 5; // Измените это значение на требуемое количество философов
+
+        DiningTable table = new DiningTable(numPhilosophers);
+        table.StartDining();
+
+        Console.ReadLine();
+    }
+
+    // DiningTable - адаптивная система, для удобного изменения количества философов.
+    class DiningTable
+    {
+        private Fork[] forks;
+        private Philosopher[] philosophers;
+
+        public DiningTable(int numPhilosophers)
+        {
+            forks = new Fork[numPhilosophers];
+            philosophers = new Philosopher[numPhilosophers];
+
+            for (int i = 0; i < numPhilosophers; i++)
+            {
+                forks[i] = new Fork();
+            }
+
+            for (int i = 0; i < numPhilosophers; i++)
+            {
+                int leftForkIndex = i;
+                int rightForkIndex = (i + 1) % numPhilosophers;
+
+                if (i != numPhilosophers - 1)
+                {
+                    philosophers[i] = new Philosopher(i + 1, forks[leftForkIndex], forks[rightForkIndex]);
+                }
+                else
+                {
+                    philosophers[i] = new Philosopher(i + 1, forks[rightForkIndex], forks[leftForkIndex]);
+                }
+            }
+        }
+
+        public void StartDining()
+        {
+            Thread[] philosopherThreads = new Thread[philosophers.Length];
+
+            for (int i = 0; i < philosophers.Length; i++)
+            {
+                philosopherThreads[i] = new Thread(philosophers[i].StartDining);
+                philosopherThreads[i].Start();
+            }
+
+            foreach (var thread in philosopherThreads)
+            {
+                thread.Join();
+            }
+        }
+    }
+
+    class Fork
+    {
+        private readonly object lockObject = new object();
+        public void PickUp()
+        {
+            Monitor.Enter(lockObject);
+        }
+
+        public void PutDown()
+        {
+            Monitor.Exit(lockObject);
+        }
+    }
+
+    class Philosopher
+    {
+        private readonly int id;
+        private readonly Fork leftFork;
+        private readonly Fork rightFork;
+
+        public Philosopher(int id, Fork leftFork, Fork rightFork)
+        {
+            this.id = id;
+            this.leftFork = leftFork;
+            this.rightFork = rightFork;
+        }
+
+        public void StartDining()
+        {
+            while (true)
+            {
+                Think();
+                PickUpForks();
+                Eat();
+                PutDownForks();
+            }
+        }
+
+        private void Think()
+        {
+            int duration = random.Next(200, 800);
+            Console.WriteLine($"{id} thinks for {duration}ms");
+            Thread.Sleep(duration);
+        }
+
+        private void PickUpForks()
+        {
+            Console.WriteLine($"\t\t{id} is hungry");
+            lock (leftFork)
+            {
+                lock (rightFork)
+                {
+                }
+            }
+        }
+
+        private void Eat()
+        {
+            int duration = random.Next(200, 800);
+            Console.WriteLine($"\t\t\t\t{id} eats for {duration}ms");
+            Thread.Sleep(duration);
+        }
+
+        private void PutDownForks()
+        {
+            lock (rightFork)
+            {
+                lock (leftFork)
+                {
+                }
+            }
+        }
+    }
+}
